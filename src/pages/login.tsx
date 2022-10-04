f3ece1ab5e65959269e102498119d803df78e976
Jerald Lim <jeraldlim.2020@smu.edu.sg>
2022-10-04 19:50:22 +0800

    style: add authcendas logo with tagline

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -4,6 +4,12 @@ import { Login } from '../components/login';
 const LoginPage: NextPage = () => {
   return (
     <div className="flex flex-col h-screen w-full items-center justify-center">
+      <div className="flex flex-col mb-16">
+        <img src="/assets/authcendas.png" />
+        <div className="font-normal text-xl">
+          Customer Engagement. Made Simple.
+        </div>
+      </div>
       <Login />
     </div>
   );

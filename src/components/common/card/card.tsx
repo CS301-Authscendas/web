a1e1671d292e9b28822b62c6e99fe8f8668d67a7
yvonnelhs <yvonnefghijk@gmail.com>
2022-10-30 20:37:21 +0800

    fix rewards card bug

--- src/components/common/card/card.tsx
+++ src/components/common/card/card.tsx
@@ -7,7 +7,7 @@ export const Card: React.FC<CardProps> = ({ title, description, imgSrc }) => {
   return (
     <>
       <div className="h-72 bg-white w-1/5 rounded-lg">
-        <img className="h-1/2 w-full rounded-lg" src={imgSrc} />
+        <img className="h-min-1/2 w-full rounded-lg" src={imgSrc} />
         <p className="text-center font-semibold mt-2">{title}</p>
         <p className="text-center mt-2 mx-3">{description}</p>
       </div>

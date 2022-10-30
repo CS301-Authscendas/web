c34cdec794a338e1f6b440bcf6fc1985972e718d
yvonnelhs <yvonnefghijk@gmail.com>
2022-10-30 20:47:44 +0800

    add change organisation button

--- src/components/common/home-content/home-content.tsx
+++ src/components/common/home-content/home-content.tsx
@@ -1,3 +1,5 @@
+import Button from 'antd/lib/button';
+import { useRouter } from 'next/router';
 import { Fragment } from 'react';
 
 interface IProps {
@@ -6,10 +8,21 @@ interface IProps {
 }
 
 export const HomeContent = ({ title, children }: IProps) => {
+  const router = useRouter();
+
+  const changeOrganisation = () => {
+    router.push('/organisations');
+  };
+
   return (
-    <Fragment>
-      <div className="text-2xl font-medium mb-8">{title}</div>
+    <>
+      <div className="flex justify-between">
+        <div className="text-2xl font-medium mb-8">{title}</div>
+        <Button type="primary" ghost={true} onClick={changeOrganisation}>
+          Change organisation
+        </Button>
+      </div>
       {children}
-    </Fragment>
+    </>
   );
 };

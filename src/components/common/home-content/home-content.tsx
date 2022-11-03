ff57dc8baed2ff0c76eb2fc0eab0e02263d313ce
Felice <felicepng2010@gmail.com>
2022-11-03 15:54:24 +0800

    feat: add logout button

--- src/components/common/home-content/home-content.tsx
+++ src/components/common/home-content/home-content.tsx
@@ -9,7 +9,7 @@ interface IProps {
 }
 
 export const HomeContent = ({ title, children }: IProps) => {
-  const { userDetails } = useAuth();
+  const { logout, userDetails } = useAuth();
 
   const router = useRouter();
 
@@ -21,12 +21,13 @@ export const HomeContent = ({ title, children }: IProps) => {
     <Fragment>
       <div className="flex justify-between items-center mb-8">
         <div className="text-2xl font-medium">{title}</div>
-        <div className="flex items-center space-x-8">
+        <div className="flex items-center space-x-6">
           <div className="font-semibold text-base">
             {userDetails?.firstName} {userDetails?.lastName}
           </div>
-          <Button type="primary" ghost onClick={changeOrganisation}>
-            Change organisation
+          <Button onClick={changeOrganisation}>Change organisation</Button>
+          <Button type="primary" ghost onClick={logout}>
+            Logout
           </Button>
         </div>
       </div>
